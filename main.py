e29a61954452bb26e9e396b8bd99517e4ec0f27d
aelbialy <AhmedElBialy@Ahmeds-MacBook-Air.local>
2017-01-23 20:59:04 +0000

    formating

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ This is our main python file for Trump as a Movie for January monthly Hack
 """
 
 import requests
+
+movie = input("Enter movie name: ")
+print("This is your movie " + movie)
+movie_title_bit = 
+
 r = requests.get("http://www.omdbapi.com/?t=mall+Cop+&y=&plot=short&r=json")
-print(r.json())
+#print(r.json())
 data = r.json()
-print(data["imdbRating"])
+print("Mall Cop's Movie rating is " + str(float(data["imdbRating"])*10) + "%") 
+print("And Trump's Approval rating is (inserted data here)")
