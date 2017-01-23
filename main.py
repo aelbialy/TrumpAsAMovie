2eb34aa7fb6dab94246968bf6682ae48a543658c
aelbialy <AhmedElBialy@Ahmeds-MacBook-Air.local>
2017-01-23 20:30:49 +0000

    movie data added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 """
 This is our main python file for Trump as a Movie for January monthly Hack
 """
+
+import requests
+r = requests.get("http://www.omdbapi.com/?t=mall+Cop+&y=&plot=short&r=json")
+print(r.json())
+data = r.json()
+print(data["imdbRating"])
