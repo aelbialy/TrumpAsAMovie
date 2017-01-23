b56de227e5141df0465371f1b42129f08e2a9c46
Cat Flynn <mewtwo97@hotmail.com>
2017-01-23 21:37:23 +0000

    Add some flavour text

--- main.py
+++ main.py
@@ -6,24 +6,28 @@ import requests
 
 
 movie = input("Enter movie name: ")
-#print("This is your movie " + movie)
 movie_title_bit = movie.replace(' ', '+')
-#print(movie_title_bit)
-
-
 
 r = requests.get("http://www.omdbapi.com/?t="+movie_title_bit+"&y=&plot=short&r=json")
-#print(r.json())
 data = r.json()
-print(movie + "'s Movie rating is " + str(float(data["imdbRating"])*10) + "%") 
-print("And Trump's Approval rating is (inserted data here)")
 
+movie_rating = float(data["imdbRating"])*10
+print(movie + "'s Movie rating is " + str(movie_rating) + "%")
+
+print('pls w8...')
 r = requests.get("http://elections.huffingtonpost.com/pollster/donald-trump-favorable-rating.json")
 polls = r.json()['polls']
-latest_poll_favourable_result = polls[len(polls) - 1]['choices']['Favorable']
+trump_rating = float(polls[len(polls) - 1]['choices']['Favorable'])
 
-print("And Trump's Approval rating is " + latest_poll_favourable_result)
+print("And Trump's Approval rating is " + str(trump_rating) + '%')
 
-print(data["imdbRating"])
+if movie_rating > trump_rating:
+    print('That means ' + movie + '\'s rating is ' + str(movie_rating - trump_rating) + '% better than Trump!')
+else:
+    print('That means Trump is ' + str(trump_rating - movie_rating) + '% better than ' + movie + '!')
+
+
+
+
 
 
