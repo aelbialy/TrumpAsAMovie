754f88c0bf01b0616e36047de28fa5d4749b73e3
Cat Flynn <mewtwo97@hotmail.com>
2017-01-23 21:12:57 +0000

    Add politics API call

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ This is our main python file for Trump as a Movie for January monthly Hack
 
 import requests
 
+
 movie = input("Enter movie name: ")
 print("This is your movie " + movie)
 movie_title_bit = 
 
+
 r = requests.get("http://www.omdbapi.com/?t=mall+Cop+&y=&plot=short&r=json")
 #print(r.json())
 data = r.json()
+
+
 print("Mall Cop's Movie rating is " + str(float(data["imdbRating"])*10) + "%") 
-print("And Trump's Approval rating is (inserted data here)")
+
+r = requests.get("http://elections.huffingtonpost.com/pollster/donald-trump-favorable-rating.json")
+polls = r.json()['polls']
+latest_poll_favourable_result = polls[len(polls) - 1]['choices']['Favorable']
+
+print("And Trump's Approval rating is " + latest_poll_favourable_result)
+
+print(data["imdbRating"])
+
+
